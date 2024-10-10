a4d91bfa9a9768c09b30ba4732116763f2a0a7f0
sh20raj <shaswatraj3@gmail.com>
2024-10-10 13:31:01 +0530

    Rookus on Hero Section

--- src/components/(Landing)/HeroSection.js
+++ src/components/(Landing)/HeroSection.js
@@ -7,95 +7,95 @@ export function HeroParallaxDemo() {
 }
 export const products = [
   {
-    title: "Moonbeam",
+    title: "Rookus",
     link: "#",
     thumbnail:
       "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/6fe323ef-4ad7-43ef-9a1a-a81d068f9585/width=450/00160-3032032489.jpeg",
   },
   {
-    title: "Cursor",
+    title: "Rookus",
     link: "#",
     thumbnail:
       "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/d6d7d8c2-d58b-4d53-9d0c-b3c7dbab30ae/width=450/32630360.jpeg",
   },
   {
-    title: "Rogue",
+    title: "Rookus",
     link: "#",
     thumbnail:
       "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/e1e205d3-23d8-4a8b-ad83-bf8822c8ac2e/width=450/32644566.jpeg",
   },
 
   {
-    title: "Editorially",
+    title: "Rookus",
     link: "#",
     thumbnail:
       "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/8a03fc2f-3839-44fd-8dec-92d1b31657d0/width=450/33330261.jpeg",
   },
   {
-    title: "Editrix AI",
+    title: "Rookus",
     link: "#",
     thumbnail:
       "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/8a03fc2f-3839-44fd-8dec-92d1b31657d0/width=450/33330261.jpeg",
   },
   {
-    title: "Pixel Perfect",
+    title: "Rookus",
     link: "#",
     thumbnail:
       "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/46a65eb3-f432-43fb-862e-5706566536e0/width=450/32787753.jpeg",
   },
 
   {
-    title: "Algochurn",
-    link: "https://algochurn.com",
+    title: "Rookus",
+    link: "#",
     thumbnail:
       "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/5c668720-826a-4c75-8a51-1f4f9c030b60/width=450/00158-4250777637.jpeg",
   },
   {
-    title: "Aceternity UI",
-    link: "https://ui.aceternity.com",
+    title: "Rookus",
+    link: "#",
     thumbnail:
       "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/efb160ec-325e-46b2-b478-93be246d361b/original=true,quality=90/32683111.jpeg",
   },
   {
-    title: "Tailwind Master Kit",
-    link: "https://tailwindmasterkit.com",
+    title: "Rookus",
+    link: "#",
     thumbnail:
       "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/6fe323ef-4ad7-43ef-9a1a-a81d068f9585/width=450/00160-3032032489.jpeg",
   },
   {
-    title: "SmartBridge",
-    link: "https://smartbridgetech.com",
+    title: "Rookus",
+    link: "#",
     thumbnail:
       "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/3482c563-eda1-469c-84fe-0de924e81f09/width=450/32938385.jpeg",
   },
   {
-    title: "Renderwork Studio",
-    link: "https://renderwork.studio",
+    title: "Rookus",
+    link: "#",
     thumbnail:
       "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/1f7602ec-8a60-4a6f-8800-4b13186dd187/width=450/33365152.jpeg",
   },
 
   {
-    title: "Creme Digital",
-    link: "https://cremedigital.com",
+    title: "Rookus",
+    link: "#",
     thumbnail:
       "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/246123a9-5297-4dc1-b8d7-c3883be255ec/width=450/36344-3671995386-1boy,ryu,ryu%20(street%20fighter),white%20dougi,solo,male%20focus,white%20headband,barefoot,red%20gloves,brown%20hair,fingerless%20gloves,torn%20c.jpeg",
   },
   {
-    title: "Golden Bells Academy",
-    link: "https://goldenbellsacademy.com",
+    title: "Rookus",
+    link: "#",
     thumbnail:
       "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/6ffbab64-0fb4-443f-a97f-506a4121ccdb/width=450/Vintage%20comic%20book,%20strength%201.5_00001.jpeg",
   },
   {
-    title: "Invoker Labs",
-    link: "https://invoker.lol",
+    title: "Rookus",
+    link: "#",
     thumbnail:
       "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/2dad0250-a6d5-425f-94ab-88d60ed471d2/width=332/E0YX57BF8XVK1XR5ZKATFW81P0.jpeg",
   },
   {
-    title: "E Free Invoice",
-    link: "https://efreeinvoice.com",
+    title: "Rookus",
+    link: "#",
     thumbnail:
       "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/8125e1ff-c56b-4b08-8264-18093f3d121f/width=450/Test%201%20comic,%20strength%201.5_00003.jpeg",
   },
